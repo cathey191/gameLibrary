1d04e7395668c0565926ebc1637aa1afe9de9af5
catherine <cathey191@hotmail.com>
2018-10-15 11:48:06 +1300

    Started styling add games page

--- add.php
+++ add.php
@@ -4,24 +4,23 @@
 
     <div class="game-container">
       <form class="item-details" action="item.php" method="post">
-        <label for="title">Game Name</label>
-        <input type="text" name="title" value=""> <br>
+        <label for="title" class="item-title">Game Name</label>
+        <input type="text" name="title" value="">
 
-        <p>Game Type</p>
-        <input type="checkbox" id="type-bl" value="Bluffing"><label for="type-bl"> Bluffing</label>
-        <input type="checkbox" id="type-cr" value="Creative"><label for="type-cr"> Creative</label>
-        <input type="checkbox" id="type-ex" value="Exploration"><label for="type-ex"> Exploration</label>
-        <input type="checkbox" id="type-hu" value="Humor"><label for="type-hu"> Humor</label>
-        <input type="checkbox" id="type-st" value="Strategy"><label for="type-st"> Strategy</label><br>
+        <div class="div-players">
+          <label for="min-players" class="item-players">Min Players</label>
+          <input type="number" name="players-min" class="input-players players-min" value="1" maxlength="2">
+          <p class="input-players"> - </p>
+          <label for="max-players" class="item-players">Max Players</label>
+          <input type="number" name="players-max" class="input-players players-max" value="8">
+        </div>
+        <div class="clear"></div>
 
-        <label for="min-players">Min Players</label>
-        <input type="number" name="min-players" value=""><br>
+        <label for="type">Game Type</label>
+        <input type="text" name="type"><br>
 
-        <label for="max-players">Max Players</label>
-        <input type="number" name="max-players" value=""><br>
-
-        <label for="discription">Discription</label>
-        <input type="textarea" name="textarea" value=""><br>
+        <label for="description">Description</label><br>
+        <textarea name="textarea" value="" class="game-description" rows="5"></textarea><br>
 
         <label for="photo">Photo</label>
         <input type="file" name="photo"><br>
--- edit.php
+++ edit.php
@@ -6,14 +6,18 @@
       <img class="item-img" src="img/magicmaze.png" alt="Magic Maze">
       <form class="item-details" action="item.php" method="post">
         <input class="input-title input" value="Magic Maze"><br>
-        <input type="text" name="type" class="input input-type" value="Strategy">
         <div class="div-players">
-          <input type="number" name="players-min" class="input input-players players-min" value="1" maxlength="2">
+          <input type="number" name="players-min" class="input-players" value="1" maxlength="2">
           <p class="input-players"> - </p>
-          <input type="number" name="players-max" class="input input-players players-max" value="8">
+          <input type="number" name="players-max" class="input-players" value="8">
         </div>
         <span class="clear"></span>
-        <p class="game-discription">After being stripped of all their possessions, a mage, a warrior, an elf, and a dwarf are forced to go rob the local Magic Maze shopping mall for all the equipment necessary for their next adventure. They agree to map out the labyrinth in its entirety first, then find each individual’s favorite store, and then locate the exit. In order to evade the surveillance of the guards who eyed their arrival suspiciously, all four will pull off their heists simultaneously, then dash to the exit. That's the plan anyway…but can they pull it off?<br /><br />Magic Maze is a real-time, cooperative game. Each player can control any hero in order to make that hero perform a very specific action, to which the other players do not have access: Move north, explore a new area, ride an escalator… All this requires rigorous cooperation between the players in order to succeed at moving the heroes prudently. However, you are allowed to communicate only for short periods during the game; the rest of the time, you must play without giving any visual or audio cues to each other. If all of the heroes succeed in leaving the shopping mall in the limited time allotted for the game, each having stolen a very specific item, then everyone wins together.<br /><br />At the start of the game, you have only three minutes in which to take actions. Hourglass spaces you encounter along the way give you more time. If the sand timer ever completely runs out, all players lose the game: Your loitering has aroused suspicion, and the mall security guards nab you!</p>
+        <input type="text" name="type" class="input-type" value="Strategy">
+        <textarea class="game-description" rows="10">After being stripped of all their possessions, a mage, a warrior, an elf, and a dwarf are forced to go rob the local Magic Maze shopping mall for all the equipment necessary for their next adventure. They agree to map out the labyrinth in its entirety first, then find each individual’s favorite store, and then locate the exit. In order to evade the surveillance of the guards who eyed their arrival suspiciously, all four will pull off their heists simultaneously, then dash to the exit. That's the plan anyway…but can they pull it off?
+
+Magic Maze is a real-time, cooperative game. Each player can control any hero in order to make that hero perform a very specific action, to which the other players do not have access: Move north, explore a new area, ride an escalator… All this requires rigorous cooperation between the players in order to succeed at moving the heroes prudently. However, you are allowed to communicate only for short periods during the game; the rest of the time, you must play without giving any visual or audio cues to each other. If all of the heroes succeed in leaving the shopping mall in the limited time allotted for the game, each having stolen a very specific item, then everyone wins together.
+
+At the start of the game, you have only three minutes in which to take actions. Hourglass spaces you encounter along the way give you more time. If the sand timer ever completely runs out, all players lose the game: Your loitering has aroused suspicion, and the mall security guards nab you!</textarea>
         <button class="btn btn-green" input="submit">Change</button>
         <div class="clear"></div>
       </form>
