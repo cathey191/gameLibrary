f8731395ad946c6114dbbcfae9b94c9044e31a65
catherine <cathey191@hotmail.com>
2018-10-17 08:45:27 +1300

    All Games page displaying data from the database

--- list.php
+++ list.php
@@ -1,43 +1,31 @@
 <?php
 
   $page = 'All Games';
-
   require ('templates/header.php');
+
+  $sql = "SELECT * FROM `board_games` WHERE 1";
+  $result = mysqli_query($dbc, $sql);
+
+  if ($result) {
+    $games = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  } else {
+    die('Error! Unable to fetch games');
+  }
 ?>
 
     <div class="item-container">
-      <div class="item">
-        <img class="item-img" src="img/magicmaze.png" alt="Magic Maze">
-        <div class="item-details">
-          <a class="item-title" href="item.php">Magic Maze</a>
-          <p class="item-players">1 - 8</p>
-          <p class="item-type">Strategy</p>
-        </div>
-      </div>
-      <div class="item">
-        <img class="item-img" src="img/magicmaze.png" alt="Magic Maze">
-        <div class="item-details">
-          <a class="item-title" href="item.php">Magic Maze</a>
-          <p class="item-players">1 - 8</p>
-          <p class="item-type">Strategy</p>
-        </div>
-      </div>
-      <div class="item">
-        <img class="item-img" src="img/magicmaze.png" alt="Magic Maze">
-        <div class="item-details">
-          <a class="item-title" href="item.php">Magic Maze</a>
-          <p class="item-players">1 - 8</p>
-          <p class="item-type">Strategy</p>
-        </div>
-      </div>
-      <div class="item">
-        <img class="item-img" src="img/magicmaze.png" alt="Magic Maze">
-        <div class="item-details">
-          <a class="item-title" href="item.php">Magic Maze</a>
-          <p class="item-players">1 - 8</p>
-          <p class="item-type">Strategy</p>
-        </div>
-      </div>
+      <?php if ($games): ?>
+        <?php foreach ($games as $game): ?>
+          <div class="item">
+            <img class="item-img" src="img/uploads/<?= $game['image_name'] ?>" alt="<?= $game['image_name'] ?>">
+            <div class="item-details">
+              <a class="item-title" href="item.php"><?= $game['title'] ?></a>
+              <p class="item-players"><?= $game['min_players'] ?> - <?= $game['max_players'] ?></p>
+              <p class="item-type"><?= $game['type'] ?></p>
+            </div>
+          </div>
+        <?php endforeach; ?>
+      <?php endif; ?>
     </div>
 
   </body>
